9266c41f8df4e60764d3638b2c307ebdf905726a
Misha Behersky <bmwant@gmail.com>
2021-10-08 16:06:21 +0300

    Add instance based tree generator :vibration_mode:

--- invert_binary_tree_v1.py
+++ invert_binary_tree_v1.py
@@ -3,7 +3,7 @@ from typing import Optional
 from tree import Node, print_tree
 
 
-class GenTree(object):
+class GenTreeClass(object):
     counter = 0
 
     @classmethod
@@ -25,6 +25,22 @@ class GenTree(object):
         return node
 
 
+class GenTreeInstance(object):
+    def __init__(self):
+        self.counter = 0
+
+    def reset(self):
+        self.counter = 0
+
+    def generate(self, levels: int) -> Optional[Node]:
+        if levels:
+            self.counter += 1
+            node = Node(self.counter)
+            node.left = self.generate(levels-1)
+            node.right = self.generate(levels-1)
+            return node
+
+
 counter = 0
 def generate_tree(levels: int) -> Optional[Node]:
     global counter
@@ -38,13 +54,30 @@ def generate_tree(levels: int) -> Optional[Node]:
     return node
 
 
-if __name__ == '__main__':
-    generator = GenTree()
+
+def test_gen_tree_class():
+    generator = GenTreeClass()
     tree1 = generator.generate(levels=3)
-    print(f'Counter between calls {GenTree.counter}')
+    print(f'Counter between calls {GenTreeClass.counter}')
     tree2 = generator.generate(levels=2)
     print('First tree')
     print_tree(tree1)
     print('Second tree')
     print_tree(tree2)
-    print(f'Counter in the end {GenTree.counter}')
+    print(f'Counter in the end {GenTreeClass.counter}')
+
+
+def test_gen_tree_instance():
+    generator = GenTreeInstance()
+    tree1 = generator.generate(levels=3)
+    generator.reset()
+    tree2 = generator.generate(levels=2)
+    print('First tree')
+    print_tree(tree1)
+    print('Second tree')
+    print_tree(tree2)
+
+
+if __name__ == '__main__':
+    # test_gen_tree_class()
+    test_gen_tree_instance()
