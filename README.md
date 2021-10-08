25e603895f612a7aaf219e57f799a66ebfaa042d
Misha Behersky <bmwant@gmail.com>
2021-10-08 11:58:45 +0300

    Add invert binary tree scratch :shirt:

---
+++ README.md
@@ -0,0 +1,10 @@
+## jaaam
+
+The more `aaaa` the better.
+
+
+```bash
+$ pyenv virtualenv 3.10.0 jaaam
+$ pyenv activate jaaam
+$ python invert_binary_tree.py
+```
---
+++ invert_binary_tree.py
@@ -0,0 +1,22 @@
+
+
+
+class Node(object):
+    pass
+
+
+class Tree(object):
+    def __init__(self, root: Node = None):
+        self.root = root
+
+
+def generate_tree(levels: int) -> Tree:
+    pass
+
+
+def print_tree(tree: Tree):
+    pass
+
+
+def invert_tree(tree: Tree) -> Tree:
+    pass
