86d68ce55d5be92b353f450a6f64ca5134ed59a1
Misha Behersky <bmwant@gmail.com>
2021-10-08 13:24:28 +0300

    Add implementation with mutable function parameter :paperclip:

--- invert_binary_tree.py
+++ invert_binary_tree.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Node(object):
     def __init__(self, value: int = None):
         self.value = value
@@ -8,7 +10,7 @@ class Node(object):
         return f'{self.value}'
 
 
-def generate_tree_(node: Node, levels: int) -> Node:
+def generate_tree_inner(node: Node, levels: int) -> Node:
     counter = 0
 
     def gen_inner(node: Node, levels: int) -> Node:
@@ -25,13 +27,32 @@ def generate_tree_(node: Node, levels: int) -> Node:
     return gen_inner(node, levels)
 
 
-def generate_tree(node: Node, levels: int, counter) -> Node:
+counter = 0
+def generate_tree_global(node: Node, levels: int) -> Node:
+    global counter
+    counter += 1
     if levels == 0:
         return Node(counter)
 
     node.value = counter
-    node.left = generate_tree(Node(), levels-1, counter+1)
-    node.right = generate_tree(Node(), levels-1, counter+1)
+    node.left = generate_tree_global(Node(), levels-1)
+    node.right = generate_tree_global(Node(), levels-1)
+    return node
+
+
+def generate_tree_mutable(node: Node, levels: int, counter: List[int] = [0]):
+    """
+    NOTE: Do not rely on such a code.
+    Exploiting the fact that list element will point to the same variable between
+    function invocations.
+    """
+    counter[0] += 1
+    if not levels:
+        return Node(counter[0])
+
+    node.value = counter[0]
+    node.left = generate_tree_mutable(Node(), levels-1, counter)
+    node.right = generate_tree_mutable(Node(), levels-1, counter)
     return node
 
 
@@ -50,7 +71,9 @@ def invert_tree(root: Node) -> Node:
 
 if __name__ == '__main__':
     root = Node()
-    tree = generate_tree(root, 2)
+    # tree = generate_tree_global(root, 2)
+    # tree = generate_tree_mutable(root, 2)
+    tree = generate_tree_inner(root, 2)
     print('Initial tree')
     print_tree(tree)
     print('Inverted tree')
