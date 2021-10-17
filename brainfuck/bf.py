df9e48ea123adbb36ae83d3d736f6f4664a1c4cb
Misha Behersky <bmwant@gmail.com>
2021-10-17 17:07:34 +0300

    Add main interpreter loop :angry:

--- brainfuck/bf.py
+++ brainfuck/bf.py
@@ -1,16 +1,16 @@
-from itertools import filterfalse
-
 class BrainFuck(object):
 
+    MEM_CELLS = 30_000  # as per original implementation
     CHARACTERS = '><+-.,[]'
 
     def __init__(self):
         self._source : str = ''
+        self._pointer = 0
+        self._memory = [0 for _ in range(self.MEM_CELLS)]
 
     def load(self, filepath):
         with open(filepath) as f:
             self._source = self.strip_comments(f.read())
-            print(self._source)
 
     @staticmethod
     def strip_comments(code: str):
@@ -22,12 +22,31 @@ class BrainFuck(object):
         )
 
     def run(self):
-        pass
+        for i, c in enumerate(self._source):
+            match c:
+                case '>':
+                    self._pointer += 1
+                case '<':
+                    self._pointer -= 1
+                case '+':
+                    raise NotImplementedError('+ is not here yet')
+                case '-':
+                    raise NotImplementedError('- is not here yet')
+                case '.':
+                    raise NotImplementedError('+ is not here yet')
+                case ',':
+                    raise NotImplementedError('+ is not here yet')
+                case '[':
+                    raise NotImplementedError('+ is not here yet')
+                case ']':
+                    raise NotImplementedError('+ is not here yet')
+                case _:
+                    raise ValueError(f'Wrong command {c} is found!')
 
 
 def main():
     interpreter = BrainFuck()
-    interpreter.load('./examples/hello_world.bf')
+    interpreter.load('./examples/hello_world.b')
     interpreter.run()
 
 
