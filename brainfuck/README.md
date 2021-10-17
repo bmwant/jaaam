89163ed8c6553376f80094f83b4835827c05a71d
Misha Behersky <bmwant@gmail.com>
2021-10-17 16:29:50 +0300

    Add brainfuck interpreter subproject :cn:

---
+++ brainfuck/README.md
@@ -0,0 +1,4 @@
+Interpreter for the [Brainfuck](https://esolangs.org/wiki/Brainfuck) programming language written in Python
+
+
+### Usage
---
+++ brainfuck/bf.py
@@ -0,0 +1,21 @@
+
+
+class BrainFuck(object):
+    def __init__(self):
+        pass
+
+    def load(self):
+        pass
+
+    def run(self):
+        pass
+
+
+def main():
+    interpreter = BrainFuck()
+    interpreter.load('./examples/hello_world.bf')
+    interpreter.run()
+
+
+if __name__ == '__main__':
+    main()
