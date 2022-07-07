57e398fc8e352894545a0cbe3ab5a3cb4dc0ba78
Misha Behersky <bmwant@gmail.com>
2022-07-07 13:55:29 +0300

    Add ip stringer excercise :maple_leaf:

---
+++ go/ip_stringers.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type IPAddr [4]byte
+
+// TODO: Add a "String() string" method to IPAddr.
+func (ip IPAddr) String() string {
+	strArr := []string{}
+	for _, v := range ip {
+		strArr = append(strArr, fmt.Sprintf("%d", v))
+	}
+	return strings.Join(strArr, ".")
+	// return fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3])
+}
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
