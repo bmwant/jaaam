5076a34697b7b3911fd7cdaab203b9e04d914929
Misha Behersky <bmwant@gmail.com>
2022-07-07 15:27:22 +0300

    Add rot13 exercise :whale2:

---
+++ go/rot13.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func rot13(b byte) byte {
+	var a, z byte
+	switch {
+	case 'a' <= b && b <= 'z':
+		a, z = 'a', 'z'
+	case 'A' <= b && b <= 'Z':
+		a, z = 'A', 'Z'
+	default:
+		return b
+	}
+	return (b-a+13)%(z-a+1) + a
+}
+
+func (r rot13Reader) Read(arr []byte) (int, error) {
+	n, err := r.r.Read(arr)
+	for i := 0; i < n; i++ {
+		arr[i] = rot13(arr[i])
+	}
+	return n, err
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
