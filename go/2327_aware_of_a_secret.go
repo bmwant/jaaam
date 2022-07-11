6d7c8bb7ed17988d127e2ed8ec044221012d16e4
Misha Behersky <bmwant@gmail.com>
2022-07-11 21:35:14 +0200

    Fix solution :sushi:

--- go/2327_aware_of_a_secret.go
+++ go/2327_aware_of_a_secret.go
@@ -9,10 +9,29 @@ func sum(arr []int) (res int) {
 	return
 }
 
+func peopleAwareOfSecret2(n int, delay int, forget int) int {
+	f := make([]int, n)
+	mod := int(10e9 + 7)
+	f[0] = 1
+	for i := delay; i < n; i++ {
+		start := i - forget + 1
+		if start < 0 {
+			start = 0
+		}
+		end := i - delay + 1
+		sharedTo := sum(f[start:end]) % mod
+		// fmt.Printf("index: %d, start: %d, end: %d, sum: %d\n", i, start, end, sharedTo)
+
+		f[i] = sharedTo
+		// fmt.Println(f, f[start:end]
+	}
+	return sum(f[n-forget:]) % mod
+}
+
 func peopleAwareOfSecret(n int, delay int, forget int) int {
 	f := make([]int, forget)
 	f[0] = 1
-	// fmt.Printf("index: %d, start: %d, end: %d\n", i, start, end)
+	fmt.Println("day 1", f, "shared", 0)
 	for i := 1; i < n; i++ {
 		// fmt.Println(i+1, "shared to", sm)
 		f = append([]int{0}, f...)
@@ -27,6 +46,8 @@ func peopleAwareOfSecret(n int, delay int, forget int) int {
 }
 
 func main() {
-	fmt.Println(peopleAwareOfSecret(6, 2, 4)) // 5
-	// fmt.Println(peopleAwareOfSecret(4, 1, 3)) // 6
+	// fmt.Println(peopleAwareOfSecret2(6, 2, 4)) // 5
+	fmt.Println(peopleAwareOfSecret2(20, 2, 4)) // 5
+	// fmt.Println(peopleAwareOfSecret2(4, 1, 3)) // 6
+	fmt.Println(peopleAwareOfSecret2(684, 18, 496)) // 653668527
 }
--- go/hello.go
+++ go/hello.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
 	res := 237 % int(10e9+7)
 	fmt.Printf("%d\n", res)
+	fmt.Println("max int64", math.MaxInt64)
 }
