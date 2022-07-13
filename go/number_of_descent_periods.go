cb8186f5c6de3f3f34aa472e0363eac19e015c95
Misha Behersky <bmwant@gmail.com>
2022-07-13 21:45:56 +0200

    Just a daily commit :hocho:

---
+++ go/number_of_descent_periods.go
@@ -0,0 +1,13 @@
+package main
+
+import fmt
+
+func getDescentPeriods(prices []int) int64 {
+    
+}
+
+func main() {
+	fmt.Println(getDescentPeriods([]int{3,2,1,4})) // 7
+	fmt.Println(getDescentPeriods([]int{8,6,7,7})) // 4
+	fmt.Println(getDescentPeriods([]int{1})) // 1
+}
